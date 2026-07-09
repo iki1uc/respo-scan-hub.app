3a7737ad8144501ddabd662e1c041e7f9a4abfa3
iki1uc <entertrainer@web.de>
2026-07-09 05:47:44 +0200

    Update scan-position.js

--- modules/scan-position.js
+++ modules/scan-position.js
@@ -1,1 +1,8 @@
+export const SCAN_POSITION = {
+    run() {
+        const pos = { x: window.scrollX, y: window.scrollY };
+        console.log("Position-Scan:", pos);
+        return pos;
+    }
+};
 
