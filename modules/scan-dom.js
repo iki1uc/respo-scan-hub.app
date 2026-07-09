6fc3d48737cab091b8d1648362d1abb05ffb4bbe
iki1uc <entertrainer@web.de>
2026-07-09 05:46:58 +0200

    Update scan-dom.js

--- modules/scan-dom.js
+++ modules/scan-dom.js
@@ -1,1 +1,8 @@
+export const SCAN_DOM = {
+    run() {
+        const elements = document.querySelectorAll('*');
+        console.log("DOM-Scan:", elements.length, "Elemente gefunden");
+        return elements.length;
+    }
+};
 
