a4f7288b91b5512d9219a07c909a3768065fb30d
iki1uc <entertrainer@web.de>
2026-07-09 05:55:40 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,8 @@
+self.addEventListener("install", () => {
+  console.log("Service Worker installiert");
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request));
+});
 
