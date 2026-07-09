8bac39a1000e4f01d57cbee246b7770be4db67d4
iki1uc <entertrainer@web.de>
2026-07-09 05:53:22 +0200

    Update router.js

--- router.js
+++ router.js
@@ -1,1 +1,19 @@
+import { SCAN_SYSTEM } from "./modules/scan-system.js";
+
+const Router = {
+    route(target) {
+        console.log("SCAN-HUB Router:", target);
+
+        switch(target) {
+            case 'scan':
+                SCAN_SYSTEM.init();
+                break;
+
+            default:
+                console.log("Unbekanntes Ziel:", target);
+        }
+    }
+};
+
+export default Router;
 
