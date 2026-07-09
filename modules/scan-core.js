804309599463a831666e5344266b1613403c2eed
iki1uc <entertrainer@web.de>
2026-07-09 05:48:14 +0200

    Add SCAN_CORE with start method and routing

--- modules/scan-core.js
+++ modules/scan-core.js
@@ -1,1 +1,9 @@
+import { SCAN_ROUTER } from "./scan-router.js";
+
+export const SCAN_CORE = {
+    start() {
+        console.log("SCAN HUB aktiv");
+        return SCAN_ROUTER.route("dom");
+    }
+};
 
