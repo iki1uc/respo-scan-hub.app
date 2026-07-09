52ce74990df8d87ad9663bfe135267c609686167
iki1uc <entertrainer@web.de>
2026-07-09 05:47:57 +0200

    Update scan-router.js

--- modules/scan-router.js
+++ modules/scan-router.js
@@ -1,1 +1,16 @@
+import { SCAN_DOM } from "./scan-dom.js";
+import { SCAN_ARG } from "./scan-arg.js";
+import { SCAN_POSITION } from "./scan-position.js";
+
+export const SCAN_ROUTER = {
+    route(type, payload) {
+        switch(type) {
+            case "dom": return SCAN_DOM.run();
+            case "arg": return SCAN_ARG.run(payload);
+            case "pos": return SCAN_POSITION.run();
+            default:
+                console.log("Unbekannter Scan:", type);
+        }
+    }
+};
 
